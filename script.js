07adf5f38169026ac69d58edb8908e62cfe6a92f
ErdiKrenzi <erdikrenzi10@gmail.com>
2024-12-18 16:49:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+//  efektet e animacioneve
+document.addEventListener("DOMContentLoaded", function () {
+    // (Animate on Scroll)
+    AOS.init({
+        duration: 1000, // Kohëzgjatja e animacionit në milisekonda
+        easing: "ease-in-out", // Stil i animacionit
+        once: true, // Animacioni luhet vetëm një herë kur kalon në ekran
+    });
+});
